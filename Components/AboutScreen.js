e7f2f4277a73511dc1e13d7f2ccfb24161c86aa9
GodDamnGitHub <39892107+GodDamnGitHub@users.noreply.github.com>
2021-12-03 00:08:36 -0500

    Update AboutScreen.js

--- Components/AboutScreen.js
+++ Components/AboutScreen.js
@@ -19,14 +19,14 @@ function AboutScreen() {
       <MarkdownView>
         # About the Project{'\n'}
         {'\n'}
-        This is a sudoku app based on react native. Create your account, log in, and enjoy the game. There is a hint function to tell you where the conflics are when You will enter a wrong number. If finished, press the submit button and your score will be recorded. Check both local and global high scores on High Score page.  An express server which interacts with MongoDB is deployed to heroku so players all around the world can play this game. Runs on both webpage and iOS.
+        This is a sudoku app based on react native. Create your account, log in, and enjoy the game. There is a hint function to tell you where the conflics are when you enter a wrong number. If finished, press the submit button and your score will be recorded. Check both local and global high scores on High Score page.  An express server interacting with MongoDB is deployed to heroku so players all around the world can play this game. Runs on both webpage and iOS.
         {'\n'}
         {'\n'}
 
-        ![RN Logo](https://github.com/GodDamnGitHub/Sudoku/blob/CPA5/img/e1.png?raw=true){'\n'}
-        ![RN Logo](https://github.com/GodDamnGitHub/Sudoku/blob/CPA5/img/e2.png?raw=true){'\n'}
-        ![RN Logo](https://github.com/GodDamnGitHub/Sudoku/blob/CPA5/img/e3.png?raw=true){'\n'}
-        ![RN Logo](https://github.com/GodDamnGitHub/Sudoku/blob/CPA5/img/e4.png?raw=true){'\n'}
+        ![RN Logo](https://github.com/GodDamnGitHub/Sudoku/blob/CPA5/img/e01.png?raw=true =336x708){'\n'}
+        ![RN Logo](https://github.com/GodDamnGitHub/Sudoku/blob/CPA5/img/e02.png?raw=true =336x708){'\n'}
+        ![RN Logo](https://github.com/GodDamnGitHub/Sudoku/blob/CPA5/img/e03.png?raw=true =336x708){'\n'}
+        ![RN Logo](https://github.com/GodDamnGitHub/Sudoku/blob/CPA5/img/e04.png?raw=true =336x708){'\n'}
         {'\n'}
         # Built With {'\n'}
           * React {'\n'}
